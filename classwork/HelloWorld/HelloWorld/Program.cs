e841c0b314ad7a9b145f983f715f9b6a9bf24d23
Kenneth Pascua <kenneth.pascua@my.tccd.edu>
2022-08-24 21:36:12 -0500

    First commit; "HelloWorld" and "Section1"

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿// Kenneth V. Pascua
+// ITSE 1430, Fall 2022
+// Lab 1
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        //A comment
+        /*
+         * A multi-line comment
+         */
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+
+            //another block of code
+        }
+    }
+}
