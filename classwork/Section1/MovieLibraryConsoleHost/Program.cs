8d864bf1c9024a79310423a65eed975b100237b4
Kenneth Pascua <kenneth.pascua@my.tccd.edu>
2022-08-29 21:05:28 -0500

    Started movie library project

---
+++ classwork/Section1/MovieLibraryConsoleHost/Program.cs
@@ -0,0 +1,20 @@
+﻿//Movie Definition
+string title = "";
+string description = "";
+int runLength = 0; //in minutes
+int releaseYear = 1900;
+string rating = "";
+bool isClassic = false;
+
+//string title = "";
+Console.WriteLine("Enter a title: ");
+
+//string description = "";
+Console.WriteLine("Enter an optional description: ");
+
+//int runLength = 0; //in minutes
+Console.WriteLine("Enter a run length (in minutes): ");
+Console.WriteLine("Enter the release year: ");
+Console.WriteLine("Entering MPAA rating: ");
+Console.WriteLine("Is this a classic? ");
+
