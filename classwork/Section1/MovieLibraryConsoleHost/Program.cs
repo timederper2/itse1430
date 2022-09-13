175f924769a1fc0f95e555ae38a63051dca1be76
Kenneth Pascua <kenneth.pascua@my.tccd.edu>
2022-09-12 21:42:46 -0500

    Completed basic functions of movieLibrary function; expansion of string explanations in Section1

--- classwork/Section1/MovieLibraryConsoleHost/Program.cs
+++ classwork/Section1/MovieLibraryConsoleHost/Program.cs
@@ -5,48 +5,190 @@ int runLength = 0; //in minutes
 int releaseYear = 1900;
 string rating = "";
 bool isClassic = false;
+bool done = false;
 
-AddMovie();
-
-int ReadInt32 ( string message )
+DisplayInformation();
+do
 {
-    Console.Write(message);
+    MenuOption input = DisplayMenu();
+    Console.WriteLine();
+    switch (input)
+    {
+        case MenuOption.Add:
+        {
+            AddMovie();
+            break;
+        }
+        case MenuOption.Edit: EditMovie(); break;
+        case MenuOption.View: ViewMovie(); break;
+        case MenuOption.Delete: DeleteMovie(); break;
+        case MenuOption.Quit: done = true; break;
 
-    string value = Console.ReadLine();
+        default: break;
+    }
+    //if (input == 'A')
+    //    AddMovie();
+    //else if (input == 'E')
+    //    EditMovie();
+    //else if (input == 'V')
+    //    ViewMovie();
+    //else if (input == 'D')
+    //    DeleteMovie();
+    //else if (input == 'Q')
+    //    break;
+} while (!done);
 
-    //Inline variable declarations
-    //int result;
-    //if (Int32.TryParse(value, out result))
-    if (Int32.TryParse(value, out int result))
-        return result;
 
-    //TODO: Loop
-    return -1;
+
+
+///Here be Functions
+///Functional Code be Starting Here
+
+void DisplayInformation ()
+{
+    Console.WriteLine("Movie Library");
+    Console.WriteLine("Kenneth Velasco Pascua, ITSE 1430");
+    Console.WriteLine("Fall 2022");
 }
 
-string ReadString ( string message )
+MenuOption DisplayMenu ()
+{
+    Console.WriteLine();
+    Console.WriteLine("A)dd Movie");
+    Console.WriteLine("E)dit Movie");
+    Console.WriteLine("V)iew Movie");
+    Console.WriteLine("D)elete Movie");
+    Console.WriteLine("Q)uit");
+
+    do
+    {
+        ConsoleKeyInfo key = Console.ReadKey(true);
+
+        switch (key.Key)
+        {
+            case ConsoleKey.A: return MenuOption.Add;
+            case ConsoleKey.E: return MenuOption.Edit;
+            case ConsoleKey.V: return MenuOption.View;
+            case ConsoleKey.D: return MenuOption.Delete;
+            case ConsoleKey.Q: return MenuOption.Quit;
+        };
+
+        //if (key.Key == ConsoleKey.A)
+        //    return 'A';
+        //else if (key.Key == ConsoleKey.E)
+        //    return 'E';
+        //else if (key.Key == ConsoleKey.V)
+        //    return 'V';
+        //else if (key.Key == ConsoleKey.D)
+        //    return 'D';
+        //else if (key.Key == ConsoleKey.Q)
+        //    return 'Q';
+    } while (true);
+}
+
+bool ReadBoolean (string message )
+{
+    Console.Write( message );
+
+    //Lookin for Y/N
+    do { 
+    ConsoleKeyInfo key = Console.ReadKey();
+    if (key.Key == ConsoleKey.Y)
+        return true;
+    else if (key.Key == ConsoleKey.N)
+        return false;
+    } while (true);
+}
+
+int ReadInt32 ( string message, int minimumValue , int maximumValue)
 {
     Console.Write(message);
 
-    string value = Console.ReadLine();
+    do
+    {
+        string value = Console.ReadLine();
 
-    return value;
+        //Inline variable declarations    
+        //int result;
+        //if (Int32.TryParse(value, out result))
+        if (Int32.TryParse(value, out int result))
+        {
+            if (result >= minimumValue && result <= maximumValue)
+                return result;
+        };
+
+        //if (false)
+        //break;  //Exit loop
+        //continue; //Exit iteration
+
+        Console.WriteLine("Value must be between " + minimumValue + " and " + maximumValue);
+    } while (true);
+}
+
+string ReadString ( string message , bool required)
+{
+    //message = "Bob";
+    Console.Write(message);
+
+    while (true)
+    {
+        string value = Console.ReadLine();
+
+        //if value is not empty or not required
+        if (value != "" || !required)
+            return value;
+
+        // Value is empty and required
+        Console.WriteLine("Value is required");
+    };
 }
 
 void AddMovie ()
 {
     //string title = "";
-    //Console.Write("Enter a title: "); //Console.Write doesn't put a new line after itself, unlike Console.Writeline
-    //title = Console.ReadLine();
-    title = ReadString("Enter a title: ");
+    title = ReadString("Enter a title: ", true);
 
     //string description = "";
-    description = ReadString("Enter an optional description: ");
+    description = ReadString("Enter an optional description: ", false);
 
     //int runLength = 0; //in minutes
-    runLength = ReadInt32("Enter a run length (in minutes): ");
+    runLength = ReadInt32("Enter a run length (in minutes): ", 0, 300);
 
-    releaseYear = ReadInt32("Enter the release year: ");
-    rating = ReadString("Entering MPAA rating: ");
-    Console.WriteLine("Is this a classic?: ");
+    releaseYear = ReadInt32("Enter the release year: ", 1900, 2100);
+    rating = ReadString("Entering MPAA rating: ", true);
+
+    isClassic = ReadBoolean("Is this a classic? ");
+}
+
+void DeleteMovie ()
+{
+    // No movie
+    if (title == "")
+        return;
+    //Not confirmed
+    if (!ReadBoolean("Are you sure you want to delete this movie (Y/N)? "))
+        return;
+
+    // TODO: Delete Movie
+    title = "";
+}
+
+void EditMovie ()
+{}
+
+void ViewMovie ()
+{
+
+    if (title == "")
+    {
+        Console.WriteLine("No movies available");
+        return;
+    }
+
+    Console.WriteLine(title);
+    Console.WriteLine(releaseYear);
+    Console.WriteLine("Length: " + runLength + " mins");
+    Console.WriteLine("MPAA Rating: " + rating);
+    Console.WriteLine("Classic: " + isClassic);
+    Console.WriteLine(description);
 }
--- classwork/Section1/Section1/Program.cs
+++ classwork/Section1/Section1/Program.cs
@@ -1,13 +1,73 @@
-﻿int hours;
+﻿
+int hours;
 
 Console.WriteLine("Hours: ");
 string value = Console.ReadLine();
 
 hours = Int32.Parse(value);
 
-Console.WriteLine("Pay Rate: ");
+Console.WriteLine("Pay rate: ");
 value = Console.ReadLine();
 
 double payRate = Double.Parse(value);
 
 Console.WriteLine("Your pay is " + (hours * payRate));
+
+int x;
+double distanceFromMoon = 0;
+//distanceFromMoon = 0;
+char letterGrade;
+bool isPassing;
+
+string name;
+string firstName = "Bob", lastName = "Smith ";
+
+//firstName = lastName = name;
+//firstName = "Sam";
+//lastName = "Sam";
+
+//string lastName;
+
+//int iHours;
+//float fRate;
+
+//Block statement
+//{
+//    decimal payRate;
+//}
+
+//distanceFromMoon = 10.5;
+isPassing = distanceFromMoon > 0;
+
+int y = 10;
+
+Console.WriteLine(++y);
+Console.WriteLine(y);
+
+Console.WriteLine(--y);
+Console.WriteLine(y);
+
+Console.WriteLine(y++);
+Console.WriteLine(y);
+
+Console.WriteLine(y--);
+Console.WriteLine(y);
+
+//Strings
+
+string emptyString = "";
+string emptyString2 = String.Empty;
+bool areEmptyStringsEqual = emptyString == emptyString2;
+string nullString = null;
+bool isEmptyString = (emptyString == null) || (emptyString == "");
+isEmptyString = String.IsNullOrEmpty(emptyString);
+
+//Literals
+string someString = "Hello \" World";
+Console.WriteLine("Hello");
+Console.WriteLine("World");
+Console.WriteLine("Hello\nWorld");
+string filePath = "C:\\windows\\system32";
+
+//Verbatim
+filePath = @"C:\windows\system32";
